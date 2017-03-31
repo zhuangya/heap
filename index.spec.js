4d41054a1a35e2e783c2696675f347955b1d2a1a
Ya Zhuang <zhuangya@gmail.com>
2017-03-31 17:50:17 +0800

    chore: change test to snapshot tests

--- index.spec.js
+++ index.spec.js
@@ -6,36 +6,42 @@ describe("max heap (the default heap)", () => {
   const readOnlyHeap = new Heap([100, 99, 98, 60, 65, 32, 33, 3]);
 
   test("it should get parent index", () => {
-    expect(readOnlyHeap.getParentIndex(1)).toEqual(0);
-    expect(readOnlyHeap.getParentIndex(6)).toEqual(2);
+    [
+      readOnlyHeap.getParentIndex(1),
+      readOnlyHeap.getParentIndex(6)
+    ].forEach(t => expect(t).toMatchSnapshot());
   });
 
   test("it should get children indexes", () => {
-    expect(readOnlyHeap.getChildrenIndexes(1)).toEqual([3, 4]);
-    expect(readOnlyHeap.getChildrenIndexes(3)).toEqual([7]);
-    expect(readOnlyHeap.getChildrenIndexes(6)).toEqual([]);
+    [
+      readOnlyHeap.getChildrenIndexes(1),
+      readOnlyHeap.getChildrenIndexes(3),
+      readOnlyHeap.getChildrenIndexes(6)
+    ].forEach(t => expect(t).toMatchSnapshot());
   });
 
   test("it should get preferred child index", () => {
-    expect(readOnlyHeap.getPreferredChildIndex(0)).toEqual(1);
-    expect(readOnlyHeap.getPreferredChildIndex(1)).toEqual(4);
-    expect(readOnlyHeap.getPreferredChildIndex(3)).toEqual(7);
-    expect(readOnlyHeap.getPreferredChildIndex(6)).toEqual(-1);
+    [
+      readOnlyHeap.getPreferredChildIndex(0),
+      readOnlyHeap.getPreferredChildIndex(1),
+      readOnlyHeap.getPreferredChildIndex(3),
+      readOnlyHeap.getPreferredChildIndex(6)
+    ].forEach(t => expect(t).toMatchSnapshot());
   });
 
   const dirtyHeap = new Heap([100, 99, 98, 60, 65, 32, 33, 3]);
   test("swap should work", () => {
     dirtyHeap.swap(0, 1);
-    expect(dirtyHeap.payload).toEqual([99, 100, 98, 60, 65, 32, 33, 3]);
+    expect(dirtyHeap.payload).toMatchSnapshot();
     dirtyHeap.swap(1, 0);
-    expect(dirtyHeap.payload).toEqual([100, 99, 98, 60, 65, 32, 33, 3]);
+    expect(dirtyHeap.payload).toMatchSnapshot();
   });
 
   const insertHeap = new Heap([100, 99, 98, 60, 65, 32, 33, 3]);
 
   test("should insert to the correct position", () => {
     insertHeap.insert(99);
-    expect(insertHeap.payload).toEqual([100, 99, 98, 99, 65, 32, 33, 3, 60]);
+    expect(insertHeap.payload).toMatchSnapshot();
     insertHeap.insert(1999);
     expect(insertHeap.payload).toEqual([
       1999,
@@ -55,17 +61,17 @@ describe("max heap (the default heap)", () => {
 
   test("should heapifyDown", () => {
     invalidHeap.heapifyDown(2);
-    expect(invalidHeap.payload).toEqual([100, 98, 89, 64, 32, 72, 4]);
+    expect(invalidHeap.payload).toMatchSnapshot();
     invalidHeap.heapifyDown(7);
-    expect(invalidHeap.payload).toEqual([100, 98, 89, 64, 32, 72, 4]);
+    expect(invalidHeap.payload).toMatchSnapshot();
   });
 
   const tinyHeap = new Heap([2, 1]);
   const largeHeap = new Heap([100, 99, 98, 97, 96, 95, 94]);
   test("should delete", () => {
     tinyHeap.delete(0);
-    expect(tinyHeap.payload).toEqual([1]);
+    expect(tinyHeap.payload).toMatchSnapshot();
     largeHeap.delete(0);
-    expect(largeHeap.payload).toEqual([99, 97, 98, 94, 96, 95]);
+    expect(largeHeap.payload).toMatchSnapshot();
   });
 });
